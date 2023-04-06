589a2e798c2c87ed18bd2ac378c554a91e0967a6
Iniuta <innazolotkomoe@gmail.com>
2023-04-06 15:51:13 +0200

    feat: add click on hamburger

--- shelter/pages/pets/index.js
+++ shelter/pages/pets/index.js
@@ -1,49 +1,12 @@
 const hamburger = document.querySelector(".hamburger");
-const navHamburger = document.querySelector(".nav-320");
-// const hamburgerMenuLogo = document.querySelector(".hamburger-menu-logo");
-// const flexLogo = document.querySelector(".flex-logo");
-const hidden = document.querySelector(".hidden");
-// const hamburgerMenuTop = document.querySelector(".hamburger-menu-top");
-const hiddenLi = document.querySelectorAll(".ul-320 li");
+const hamburgerWrap = document.querySelector(".hamburger-wrap");
+const navigation = document.querySelector(".navigation");
 const body = document.querySelector("body");
-const header = document.querySelector(".header-flex");
-const logo = document.querySelector(".flex-logo");
 
-
-hamburger.onclick = () => {
-  hamburger.classList.toggle("hamburger-active");
-  navHamburger.classList.toggle("hamburger-active");
-  // flexLogo.classList.toggle("hamburger-active");
-  hidden.classList.toggle("hamburger-active");
-  body.classList.toggle("hamburger-active");
-  header.classList.toggle("hamburger-active");
-  logo.classList.toggle("hamburger-active");
-};
-
-hidden.onclick = () => {
-    turnOffActive();
-};
-
-
-hiddenLi.forEach((li) => {
-  li.addEventListener("click", () => {
-    turnOffActive();
+if (hamburger) {
+  hamburger.addEventListener("click", function (e) {
+    hamburger.classList.toggle("hamburger__active");
+    hamburgerWrap.classList.toggle("hamburger-wrap__active");
+    navigation.classList.toggle("navigation__active");
   });
-});
-
-function turnOffActive() {
-    hamburger.classList.remove("hamburger-active");
-    navHamburger.classList.remove("hamburger-active");
-    // flexLogo.classList.remove("hamburger-active");
-    hidden.classList.remove("hamburger-active");
-    body.classList.remove("hamburger-active");
-    header.classList.remove("hamburger-active");
-    logo.classList.remove("hamburger-active");
 }
-
-window.addEventListener("resize", function (event) {
-    var newWidth = window.innerWidth;
-    if (newWidth > 768) {
-        turnOffActive();
-    }
-  });
